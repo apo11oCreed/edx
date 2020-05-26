873418f3a4c6e9f1cb820c93ee33bd1ae61cae8b
apo11oCreed <nelson.ncdesigns@gmail.com>
2020-05-26 00:06:08 -0400

    All tests pass locally on Codio. Submitted as Education Complete

--- homework-4_react/FontChooser.js
+++ homework-4_react/FontChooser.js
@@ -2,62 +2,62 @@ class FontChooser extends React.Component {
 
     constructor(props) {
         super(props);
-
-        var boldCheckbox = document.getElementById('boldCheckbox'),
-            decreaseButton = document.getElementById('decreaseButton'),
-            increaseButton = document.getElementById('increaseButton'),
-            fontSizeSpan = document.getElementById('fontSizeSpan');
-
-        this.state = {
-            checked: false,
-            weight: false,
-            hidden: true,
-            fontSizeSpan: this.props.size,
-            fontColor: 'black'
+        var bold;
+        if (this.props.bold == 'false') {
+            bold = false;
+        } else {
+            bold = true;
         }
+        this.state = {
+            checked: bold,
+            bold: bold,
+            hidden: true,
+            reverted: false,
+            fontSizeSpan: Number(this.props.size),
+            fontColor: 'black',
+            min: this.props.min,
+            max: this.props.max
+        };
+
+
     }
 
     toggleHidden() {
 
         this.setState({ hidden: !this.state.hidden });
 
-        if (this.state.hidden == true) {
-            boldCheckbox.removeAttribute('hidden');
-            decreaseButton.removeAttribute('hidden');
-            increaseButton.removeAttribute('hidden');
-            fontSizeSpan.removeAttribute('hidden');
+        if (this.state.hidden === true) {
+            this.setState({ hidden: false });
         } else {
-            boldCheckbox.setAttribute('hidden', 'true');
-            decreaseButton.setAttribute('hidden', 'true');
-            increaseButton.setAttribute('hidden', 'true');
-            fontSizeSpan.setAttribute('hidden', 'true');
+            this.setState({ hidden: true });
         }
 
     }
 
     toggleBold() {
-        //var boldCheckbox = document.getElementById('boldCheckbox'),
-        //    textSpan = document.getElementById('textSpan');
 
-        this.setState({ checked: !this.state.checked });
+        this.setState({ bold: !this.state.bold });
 
-        this.setState({ weight: !this.state.weight });
-
-        if (this.state.checked == false) {
-            boldCheckbox.setAttribute('checked', 'true');
+        if (this.state.bold === false) {
+            this.setState({ bold: true });
         } else {
-            boldCheckbox.removeAttribute('checked');
+            this.setState({ bold: false });
         }
     }
 
     decreaseButton() {
 
-        if (Number(this.state.fontSizeSpan) > this.props.min) {
-            console.log('test2');
+        var min = this.state.min;
+
+        if (min <= 0) {
+            min = 1;
+        }
+
+        if (Number(this.state.fontSizeSpan) > min) {
             this.setState({ fontSizeSpan: --this.state.fontSizeSpan });
         }
 
-        if (this.state.fontSizeSpan == this.props.min) {
+        if (this.state.fontSizeSpan == min) {
             this.setState({ fontColor: 'red' });
         } else {
             this.setState({ fontColor: 'black' });
@@ -77,20 +77,27 @@ class FontChooser extends React.Component {
         }
     }
 
+    revertDefault() {
+        this.setState({ fontSizeSpan: Number(this.props.size) });
+        this.setState({ fontColor: 'black' });
+    }
+
 
     render() {
 
-        var weight = this.state.weight ? 'bold' : 'normal',
+        var weight = this.state.bold ? 'bold' : 'normal',
+            //checkedBox = this.state.checked ? true : false,
+            hidden = this.state.hidden ? true : false,
             fontSizeSpan = this.state.fontSizeSpan,
             fontColor = this.state.fontColor;
 
         return (
             <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true' onClick={this.toggleBold.bind(this)} />
-	       <button onClick={this.decreaseButton.bind(this)} id="decreaseButton" hidden='true'>-</button>
-	       <span style = { { fontSize: fontSizeSpan,color: fontColor } } id="fontSizeSpan" hidden='true'>{this.state.fontSizeSpan}</span>
-	       <button onClick={this.increaseButton.bind(this)} id="increaseButton" hidden='true'>+</button>
-	       <span style={{fontWeight:weight}} id="textSpan" onClick={this.toggleHidden.bind(this)}>{this.props.text}</span>
+	       <input onChange={this.toggleBold.bind(this)} type="checkbox" id="boldCheckbox" hidden={hidden} checked={this.state.bold} />
+	       <button onClick={this.decreaseButton.bind(this)} id="decreaseButton" hidden={hidden}>-</button>
+	       <span onDoubleClick={this.revertDefault.bind(this)} style={{fontWeight:weight,fontSize: fontSizeSpan,color: fontColor}} id="fontSizeSpan" hidden={hidden}>{fontSizeSpan}</span>
+	       <button onClick={this.increaseButton.bind(this)} id="increaseButton" hidden={hidden}>+</button>
+	       <span onClick={this.toggleHidden.bind(this)} style={{fontWeight:weight,fontSize: fontSizeSpan}} id="textSpan">{this.props.text}</span>
 	       </div>
         );
     }
